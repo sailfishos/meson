sailfishos/meson

commit e597dd12f8168ea4660a6fca1b346c776e0e8b55

[meson] Update to version 0.55.3. Fixes JB#51256
Fix macros.meson: `_smp_build_ncpus` is available only since rpm 4.15.
Use python3 rpm macros.

rpm/meson.spec
@@ -1,21 +1,17 @@
-%global __python %{__python3}
-
 %define libname  mesonbuild
 
 Name:           meson
-Version:        0.54.3
+Version:        0.55.3
 Release:        1
 Summary:        High productivity build system
 License:        ASL 2.0
-Url:            http://mesonbuild.com/
+Url:            https://mesonbuild.com/
 Source:         %{name}-%{version}.tar.bz2
 Patch0:         0001-patch-macros.patch
 
 BuildArch:      noarch
 
 BuildRequires:  python3-devel >= 3.5.0
-BuildRequires:  python3-setuptools
-Requires:       python3-setuptools
 Requires:       ninja >= 1.7.0
 # Workaround ccache autodetection not working on OBS arm builds, JB#42632
 Requires:       ccache
@@ -32,10 +28,10 @@ Domain Specific Language.
 %autosetup -p1 -n %{name}-%{version}/%{name}
 
 %build
-python3 setup.py build
+%py3_build
 
 %install
-python3 setup.py install --root=%{buildroot} --prefix=%{_prefix}
+%py3_install
 install -Dpm 0644 data/macros.%{name} %{buildroot}%{_sysconfdir}/rpm/macros.%{name}
 
 rm -rf %{buildroot}/%{_mandir}/*
@@ -43,8 +39,8 @@ rm -rf %{buildroot}/%{_mandir}/*
 %files
 %license COPYING
 %{_bindir}/%{name}
-%{python_sitelib}/%{libname}/
-%{python_sitelib}/%{name}-*.egg-info/
+%{python3_sitelib}/%{libname}/
+%{python3_sitelib}/%{name}-*.egg-info/
 %{_sysconfdir}/rpm/macros.%{name}
 %dir %{_datadir}/polkit-1
 %dir %{_datadir}/polkit-1/actions

rpm/0001-patch-macros.patch
@@ -1,22 +1,43 @@
 diff --git a/data/macros.meson b/data/macros.meson
-index c5b90de03..483d1c5ad 100644
+index cc4953c5f..27370b838 100644
 --- a/data/macros.meson
 +++ b/data/macros.meson
-@@ -1,3 +1,5 @@
+@@ -1,9 +1,20 @@
 +%_vpath_srcdir .
 +%_vpath_builddir _build
  %__meson %{_bindir}/meson
  %__meson_wrap_mode nodownload
  %__meson_auto_features enabled
-@@ -9,7 +11,10 @@
-         if [ "$MESON_BUILD_NCPUS" -gt 1 ]; then echo "--num-processes $MESON_BUILD_NCPUS"; fi)
  
++%_smp_meson_build_ncpus %([ -z "$RPM_BUILD_NCPUS" ] \\\
++	&& RPM_BUILD_NCPUS="`/usr/bin/getconf _NPROCESSORS_ONLN`"; \\\
++        ncpus_max=%{?_smp_ncpus_max}; \\\
++        if [ -n "$ncpus_max" ] && [ "$ncpus_max" -gt 0 ] && [ "$RPM_BUILD_NCPUS" -gt "$ncpus_max" ]; then RPM_BUILD_NCPUS="$ncpus_max"; fi; \\\
++        if [ "$RPM_BUILD_NCPUS" -gt 1 ]; then echo "$RPM_BUILD_NCPUS"; fi)
++
  %meson \
 -    %set_build_flags \
-+    export CFLAGS="${CFLAGS:-%__global_cflags}"       \
-+    export CXXFLAGS="${CXXFLAGS:-%__global_cxxflags}" \
-+    export FFLAGS="${FFLAGS:-%__global_fflags}"       \
-+    export FCFLAGS="${FCFLAGS:-%__global_fcflags}"    \
++    export CFLAGS="${CFLAGS:-%optflags}" \
++    export CXXFLAGS="${CXXFLAGS:-%optflags}" \
++    export FFLAGS="${FFLAGS:-%__global_fflags}" \
++    export FCFLAGS="${FCFLAGS:-%__global_fcflags}" \
      %{shrink:%{__meson} \
          --buildtype=plain \
          --prefix=%{_prefix} \
+@@ -27,7 +38,7 @@
+ %meson_build \
+     %{shrink:%{__meson} compile \
+         -C %{_vpath_builddir} \
+-        -j %{_smp_build_ncpus} \
++        -j %{_smp_meson_build_ncpus} \
+         --verbose \
+         %{nil}}
+ 
+@@ -40,6 +51,6 @@
+ %meson_test \
+     %{shrink:%{__meson} test \
+         -C %{_vpath_builddir} \
+-        --num-processes %{_smp_build_ncpus} \
++        --num-processes %{_smp_meson_build_ncpus} \
+         --print-errorlogs \
+         %{nil}}

commit 2803c1ddfd5b168dcbdde6de496a792c3d455fff

[meson] BuildRequire python3-setuptools. JB#54767
Do not assume python3-setuptools is preinstalled.
Fixes regression introduced by e597dd12f8168ea4660a6fca1b346c776e0e8b55.

Signed-off-by: Björn Bidar <bjorn.bidar@jolla.com>

rpm/meson.spec
@@ -10,6 +10,7 @@ Source:         %{name}-%{version}.tar.bz2
 Patch0:         0001-patch-macros.patch
 BuildArch:      noarch
 BuildRequires:  python3-devel >= 3.5.0
+BuildRequires:  python3-setuptools
 Requires:       ninja >= 1.7.0
 # Workaround ccache autodetection not working on OBS arm builds, JB#42632
 Requires:       ccache